8b0d8431230e85a9fc3cd484f66e70434faab20d
Ronaldo Silva de Oliveira <ronaldo.df@gmail.com>
2023-02-04 20:59:12 -0300

    Criando as configuracoes de rede.

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_iam_user" "user_three" {
   name = "user3"
 }
 
-# Ajustar as politicas dos grupos so-adm e db-adm
+# Ajustar as politicas dos grupos so-adm e db-adm.
 
 resource "aws_iam_group_policy_attachment" "attach_policy_group_one" {
   group      = aws_iam_group.group_one.name
@@ -85,3 +85,73 @@ resource "aws_iam_group_policy_attachment" "attach_policy_group_two" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
+# Cria o VPC xpe-vpc-mod1 e configurações de rede para provisionamento da infra.
+resource "aws_vpc" "xpe-vpc-mod1" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_subnet" "xpe-subnet-mod1-privada" {
+  vpc_id     = aws_vpc.xpe-vpc-mod1.id
+  cidr_block = "10.1.0.0/23"
+}
+
+resource "aws_subnet" "xpe-subnet-mod1-publica" {
+  vpc_id     = aws_vpc.xpe-vpc-mod1.id
+  cidr_block = "10.1.10.0/23"
+}
+
+resource "aws_internet_gateway" "xpe-gw-mod1" {
+  vpc_id = aws_vpc.xpe-vpc-mod1.id
+}
+
+resource "aws_route_table" "xpe-rt-mod1" {
+  vpc_id = aws_vpc.xpe-vpc-mod1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.xpe-gw-mod1.id
+  }
+}
+
+resource "aws_route_table_association" "xpe-rt-mod1-association" {
+  subnet_id      = aws_subnet.xpe-subnet-mod1-publica.id
+  route_table_id = aws_route_table.xpe-rt-mod1.id
+}
+
+resource "aws_security_group" "xpe-sg-mod1" {
+  name        = "xpe-sg-mod1"
+  description = "Contrala acessos aos recursos da infra."
+  vpc_id      = aws_vpc.xpe-vpc-mod1.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.xpe-vpc-mod1.cidr_block]
+  }
+
+    ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.xpe-vpc-mod1.cidr_block]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.xpe-vpc-mod1.cidr_block]
+  }
+
+  egress {
+    description      = "Permite todo trafego de saida"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
